9b4c09d7fcbeadae83f3c1bc7efcd75fb58c9ba6
ahmadbadawikr <ahmadbadawikr@outlook.com>
2020-06-09 16:38:55 +0700

    add db

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_07_140943) do
+ActiveRecord::Schema.define(version: 2020_06_08_112112) do
 
   create_table "items", force: :cascade do |t|
     t.string "title"
     t.text "notes"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "players", force: :cascade do |t|
+    t.string "name"
+    t.integer "age"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
